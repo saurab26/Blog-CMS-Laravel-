519e882db0f202c9860b3e992eb4a21c620654b3
saurab26 <saurabkarmacharya@gmail.com>
2019-01-22 16:34:54 +0545

    Display all posts to the blog index

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('blog.index');
-});
+Route::get('/', [
+    'uses'=> 'Blogcontroller@index',
+]);
+    
 
 Route::get('/blog/show', function () {
     return view('blog.show');
