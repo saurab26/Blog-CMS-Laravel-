bb4c0366ab33d63d2e74f215512995fdf0e520b4
saurab26 <saurabkarmacharya@gmail.com>
2019-01-22 14:48:39 +0545

    Create dummy data for users amd posts table

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // reset  the uses table
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
+        DB::table('users')->truncate();
+
+        // generate 3  users/author
+
+        DB::table('users')->insert([
+            [
+                'name'=>"Saurab Karmacharya",
+                'email'=>"saurabkarmacharya",
+                'password'=>bcrypt('secert')
+            ],
+            [
+                'name'=>"Admin admin",
+                'email'=>"admin@gmail.com",
+                'password'=>bcrypt('admin')
+            ],
+            [
+                'name'=>"Ram thakur",
+                'email'=>"ramthakur@gmail.com",
+                'password'=>bcrypt('ram123')
+            ],
+        ]);
+    }
+}
