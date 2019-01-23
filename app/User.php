3a8cbc201b6b5f69b0ba145c2e355a92d523fb7d
saurab26 <saurabkarmacharya@gmail.com>
2019-01-23 20:59:25 +0545

    Add author bio and gravatar

--- app/User.php
+++ app/User.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use GrahamCampbell\Markdown\Facades\Markdown;
 
 class User extends Authenticatable
 {
@@ -30,6 +31,22 @@ class User extends Authenticatable
     public function posts()
     {
         return $this->hasMany(Post::class,'author_id');
+    }
+
+    public function gravatar()
+    {
+        $email = $this->email;
+        $default = "https://cdn0.iconfinder.com/data/icons/iconshock_guys/512/andrew.png";
+        $size = 100;
+
+        return  "https://www.gravatar.com/avatar/" . md5( strtolower( trim( $email ) ) ) . "?d=" . urlencode( $default ) . "&s=" . $size;
+
+    }
+
+
+    public function getBioHtmlAttribute($value)
+    {
+        return $this->bio ? Markdown::convertToHtml(e($this->bio)): NULL;
     }
     public function getRouteKeyName()
     {
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Factory;
 
 class UsersTableSeeder extends Seeder
 {
@@ -16,25 +17,28 @@ class UsersTableSeeder extends Seeder
         DB::table('users')->truncate();
 
         // generate 3  users/author
-
+        $faker = Factory::create();
         DB::table('users')->insert([
             [
                 'name'=>"Saurab Karmacharya",
                 'slug'=>"saurab-karmacharya",
                 'email'=>"saurabkarmacharya@gmail.com",
-                'password'=>bcrypt('secert')
+                'password'=>bcrypt('secert'),
+                'bio'=>$faker->text(rand(250,300))
             ],
             [
                 'name'=>"Admin admin",
                 'slug'=>"admin-admmin",
                 'email'=>"admin@gmail.com",
-                'password'=>bcrypt('admin')
+                'password'=>bcrypt('admin'),
+                'bio'=>$faker->text(rand(250,300))
             ],
             [
                 'name'=>"Ram thakur",
                 'slug'=>"ram-thakur",
                 'email'=>"ramthakur@gmail.com",
-                'password'=>bcrypt('ram123')
+                'password'=>bcrypt('ram123'),
+                'bio'=>$faker->text(rand(250,300))
             ],
         ]);
     }
