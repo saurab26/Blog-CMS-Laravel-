e815030259dcd54cdf0b0be642d017b708eae9bb
saurab26 <saurabkarmacharya@gmail.com>
2019-01-23 22:32:29 +0545

    Count the post view and tidy up the composer service provider

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -49,6 +49,7 @@ class BlogController extends Controller
 
     public function show(Post $post)
     {
+        $post->increment('view_count');
         return view('blog.show',compact('post'));
     }
 }
